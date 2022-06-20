ece68124c65cb65a1fa8ee9a18002c4dfa811d27
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-20 08:40:32 -0300

    Service
    Implementação dos Services

---
+++ projeto-curso-spring/src/main/java/projeto/spring/service/ClienteService.java
@@ -0,0 +1,16 @@
+package projeto.spring.service;
+
+import projeto.spring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
