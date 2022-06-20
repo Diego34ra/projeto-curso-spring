ace77e86f7837fe8d09576ff247ee0b6edfc3f5e
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-20 08:21:55 -0300

    commit inicial

---
+++ projeto-curso-spring/src/main/java/projeto/spring/ProjetoCursoSpringApplication.java
@@ -0,0 +1,13 @@
+package projeto.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoSpringApplication.class, args);
+	}
+
+}
---
+++ projeto-curso-spring/src/test/java/projeto/spring/ProjetoCursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
