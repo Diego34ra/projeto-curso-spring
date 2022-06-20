b06644a867bf22882f23862e65ec05dbd6f70df9
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-20 08:47:44 -0300

    Update ProjetoCursoSpringApplication.java

--- projeto-curso-spring/src/main/java/projeto/spring/ProjetoCursoSpringApplication.java
+++ projeto-curso-spring/src/main/java/projeto/spring/ProjetoCursoSpringApplication.java
@@ -2,7 +2,9 @@ package projeto.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProjetoCursoSpringApplication {
 
